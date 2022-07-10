94c122c7586284a18789759454f13d42da489752
LikerK <nagorniykirill@gmail.com>
2022-07-10 10:26:10 +0400

    fix staticfiles

--- messager/settings.py
+++ messager/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 
 STATIC_URL = 'staticfiles'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
 VENV_PATH = os.path.dirname(BASE_DIR)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
